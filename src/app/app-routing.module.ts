0e35ac35b67da1ba12e07ad4389e86e914b3edd2
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-03-02 19:23:43 +0100

    My second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsPageModule } from './pages/about-us.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+        path: 'o-nas', 
+        loadChildren: () => AboutUsPageModule
+      }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
