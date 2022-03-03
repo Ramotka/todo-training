04dde216d3196fa4dbd370aa868493963c4a27bf
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-03-03 21:05:48 +0100

    home and about-us introduced

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsPageModule } from './pages/about-us.page-module';
+import { HomePageModule } from './pages/home.page-module';
 
 const routes: Routes = [{ 
         path: 'o-nas', 
         loadChildren: () => AboutUsPageModule
-      }];
+      },
+  { 
+        path: 'home', 
+        loadChildren: () => HomePageModule
+      }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
