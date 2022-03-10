55cd98ce278a37c39bb0bd84eabaa29e83de6148
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-03-10 11:37:20 +0100

    smart comp designers-list wstawiony do about-us

---
+++ projects/designers/src/lib/designers.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-designers',
+  template: `
+    <p>
+      designers works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DesignersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/about-us.page-module.ts
+++ src/app/pages/about-us.page-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AboutUsPage } from './about-us.page';
+import { DesignersListComponentModule } from '../../../projects/designers/src/lib/adapters/primary/ui/designers-list.component-module';
 
 @NgModule({ imports: [CommonModule, 
       RouterModule.forChild([
@@ -9,7 +10,9 @@ import { AboutUsPage } from './about-us.page';
           path: '',
           component: AboutUsPage,
         }
-      ])],
+      ]),
+  DesignersListComponentModule,
+],
   	declarations: [AboutUsPage],
   	providers: [],
   	exports: [] })
