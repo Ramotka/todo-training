3524e522ab0349f2c9e557573eb3871da4c5759f
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-03-11 22:25:45 +0100

    formularz kontaktowy koncowa wersja

--- projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
+++ projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
@@ -5,15 +5,21 @@ import { FormGroup, FormControl } from '@angular/forms';
 @Component({ selector: 'lib-contact-form', templateUrl: './contact-form.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class ContactFormComponent {
     contact = "Zadzwoń";
+    contact2 = "";
+    contact3 = "";
   readonly kontakt: FormGroup = new FormGroup({email: new FormControl(), imie: new FormControl()});
 
     onClick() {
         this.contact = "tel: +48 123-456-789";
     }
     submit(kontakt:FormGroup) {
-        this.contact = kontakt.get("email").value;
+        alert(kontakt.get("email").value);
+        this.contact2 = kontakt.get("email").value;
+        this.contact3 = kontakt.get("imie").value;
 
     }
-    onFormSubmited(kontakt: FormGroup): void {console.log(kontakt.getRawValue());}
+    // onFormSubmited(kontakt: FormGroup): void {
+    //     console.log(kontakt.getRawValue());
+    // }
 
 }
