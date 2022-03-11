a8bd050b9358ff1b7674992583cca2e890536ad7
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-03-11 21:16:35 +0100

    "Hello contact" zmienia się w numer telefonu

--- projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
+++ projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
@@ -4,8 +4,10 @@ import { Alert } from 'selenium-webdriver';
 @Component({ selector: 'lib-contact-form', templateUrl: './contact-form.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class ContactFormComponent {
     contact = "Hello contact";
+    
     onClick() {
         alert("Zadzwoń!!!")
+        this.contact = "tel. 123-456-789"
     }
 
 }
