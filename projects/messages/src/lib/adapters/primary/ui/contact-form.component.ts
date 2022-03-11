8d65efe4dc53ed336b55f844121a5c787d2ea819
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-03-11 21:42:42 +0100

    dodany formularz kontaktowy

--- projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
+++ projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
@@ -1,13 +1,20 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 import { Alert } from 'selenium-webdriver';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({ selector: 'lib-contact-form', templateUrl: './contact-form.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class ContactFormComponent {
     contact = "Hello contact";
-    
+  readonly kontakt: FormGroup = new FormGroup({email: new FormControl(), imie: new FormControl()});
+
     onClick() {
-        alert("Zadzwoń!!!")
-        this.contact = "tel. 123-456-789"
+        alert("Zadzwoń!!!");
+        this.contact = "tel. 123-456-789";
     }
+    submit(kontakt:FormGroup) {
+        this.contact = kontakt.get("email").value;
+
+    }
+    
 
 }
