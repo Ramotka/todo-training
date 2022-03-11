62c115bcc47806fad3a7b526f9f76e7e8e6124c2
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-03-11 22:06:25 +0100

    formularz kontaktowy w konsoli

--- projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
+++ projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
@@ -4,17 +4,16 @@ import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({ selector: 'lib-contact-form', templateUrl: './contact-form.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class ContactFormComponent {
-    contact = "Hello contact";
+    contact = "Zadzwoń";
   readonly kontakt: FormGroup = new FormGroup({email: new FormControl(), imie: new FormControl()});
 
     onClick() {
-        alert("Zadzwoń!!!");
-        this.contact = "tel. 123-456-789";
+        this.contact = "tel: +48 123-456-789";
     }
     submit(kontakt:FormGroup) {
         this.contact = kontakt.get("email").value;
 
     }
-    
+    onFormSubmited(kontakt: FormGroup): void {console.log(kontakt.getRawValue());}
 
 }
