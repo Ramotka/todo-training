b677bdb79672bb5f0c850f49f3816300dd6d6185
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-03-11 20:53:34 +0100

    dodany onClick i alert na "Hello contact"

--- projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
+++ projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
@@ -1,5 +1,11 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { Alert } from 'selenium-webdriver';
 
 @Component({ selector: 'lib-contact-form', templateUrl: './contact-form.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class ContactFormComponent {
+    contact = "Hello contact";
+    onClick() {
+        alert("Zadzwoń!!!")
+    }
+
 }
