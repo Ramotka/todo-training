3003487cf0e64680df1e2b7efa6ecdcdb33c092c
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-03-21 23:39:18 +0100

    RegistrationForm upgraded in HTML - test

---
+++ projects/form/src/lib/application/ports/secondary/form.dto.ts
@@ -0,0 +1,11 @@
+export interface FormDTO {
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly email: string;
+  readonly password: string;
+  readonly clientType: string;
+  readonly info: string;
+  readonly planFile: string;
+  readonly checkbox: string;
+  readonly room: string;
+}
