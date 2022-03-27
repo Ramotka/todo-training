005be490dc88d004b20628f5057ce81ed64212f7
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-03-27 19:16:27 +0200

    L7E5

--- projects/designers2/src/lib/adapters/primary/ui/designer-list2.component.ts
+++ projects/designers2/src/lib/adapters/primary/ui/designer-list2.component.ts
@@ -1,15 +1,8 @@
-import {
-  Component,
-  ViewEncapsulation,
-  ChangeDetectionStrategy,
-  Inject,
-} from "@angular/core";
-import { Observable } from "rxjs";
-import { Employee2DTO } from "../../../application/ports/secondary/employee2.dto";
-import {
-  GETS_ALL_EMPLOYEE2_DTO,
-  GetsAllEmployee2DtoPort,
-} from "../../../application/ports/secondary/gets-all-employee2.dto-port";
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, Inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee2DTO } from '../../../application/ports/secondary/employee2.dto';
+import { GETS_ALL_EMPLOYEE2_DTO, GetsAllEmployee2DtoPort } from '../../../application/ports/secondary/gets-all-employee2.dto-port';
+import { CONTEXT_DTO_STORAGE, ContextDtoStoragePort } from '../../../application/ports/secondary/context-dto.storage-port';
 
 @Component({
   selector: "lib-designer-list2",
@@ -22,6 +15,10 @@ export class DesignerList2Component {
 
   constructor(
     @Inject(GETS_ALL_EMPLOYEE2_DTO)
-    private _getsAllEmployee2Dto: GetsAllEmployee2DtoPort
+    private _getsAllEmployee2Dto: GetsAllEmployee2DtoPort, @Inject(CONTEXT_DTO_STORAGE) private _contextDtoStorage: ContextDtoStoragePort
   ) {}
+
+  onEmployeeClicked(employee: Employee2DTO): void {
+    this._contextDtoStorage.next({ employeeId: employee.id});
+  }
 }
