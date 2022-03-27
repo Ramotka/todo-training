ba3d9858a7546e32fd7f26f00939d12d8f0e4bea
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-03-27 18:46:09 +0200

    L7E3

--- projects/designers2/src/lib/adapters/primary/ui/employee-detail2.component.ts
+++ projects/designers2/src/lib/adapters/primary/ui/employee-detail2.component.ts
@@ -1,4 +1,3 @@
-import { ActivatedRoute } from "@angular/router";
 import {
   Component,
   ViewEncapsulation,
@@ -6,11 +5,17 @@ import {
   Inject,
 } from "@angular/core";
 import { Observable } from "rxjs";
+import { switchMap } from "rxjs/operators";
 import { Employee2DTO } from "../../../application/ports/secondary/employee2.dto";
 import {
   GETS_ONE_EMPLOYEE2_DTO,
   GetsOneEmployee2DtoPort,
 } from "../../../application/ports/secondary/gets-one-employee2.dto-port";
+import {
+  CONTEXT_DTO_STORAGE,
+  ContextDtoStoragePort,
+} from "../../../application/ports/secondary/context-dto.storage-port";
+import { ContextDTO } from "../../../application/ports/secondary/context.dto";
 
 @Component({
   selector: "lib-employee-detail2",
@@ -19,17 +24,30 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeDetail2Component {
-  // params$ = this._activatedRoute.params;
-
-  employee$: Observable<Employee2DTO> = this._getsOneEmployee2Dto.getOne(
-    this._activatedRoute.snapshot.params.employeeId
-    // '9OPINmbiBIuSwKr9k9zC'
-);
+  employee$: Observable<Employee2DTO> = this._contextDtoStoragePort
+    .asObservable()
+    .pipe(
+      switchMap((data: ContextDTO) => 
+      this._getsOneEmployee2Dto.getOne(data.employeeId)));
 
   constructor(
     @Inject(GETS_ONE_EMPLOYEE2_DTO)
     private _getsOneEmployee2Dto: GetsOneEmployee2DtoPort,
-    private _activatedRoute: ActivatedRoute
+    @Inject(CONTEXT_DTO_STORAGE)
+    private _contextDtoStoragePort: ContextDtoStoragePort
   ) {}
 }
+
+// export class EmployeeDetail2Component {
+
+//   employee$: Observable<Employee2DTO> = this._getsOneEmployee2Dto.getOne(
+//     this._activatedRoute.snapshot.params.employeeId
+//     // '9OPINmbiBIuSwKr9k9zC'
+// );
+
+//   constructor(
+//     @Inject(GETS_ONE_EMPLOYEE2_DTO)
+//     private _getsOneEmployee2Dto: GetsOneEmployee2DtoPort,
+//     private _activatedRoute: ActivatedRoute
+//   ) {}
 
