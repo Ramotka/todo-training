992aae5007c94e07f0debd7bb28a9a4a6d764b6c
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-03-27 18:13:48 +0200

    L7E2

--- projects/designers2/src/lib/application/ports/secondary/context-dto.storage-port.ts
+++ projects/designers2/src/lib/application/ports/secondary/context-dto.storage-port.ts
@@ -8,3 +8,8 @@ export interface ContextDtoStoragePort {
   next(item: Partial<ContextDTO | undefined>): void;
   asObservable(): Observable<ContextDTO>;
 }
+
+
+//void - zmiana stanu
+//storage dziala w pamieci przegladarki, nic się nie zapisuje w firebase
+//DTOporty sa po to, by zapisywac cos permanentnie dla wszystkich uzytkownikow w bazie danych lub odczytywać globalne dane mojej aplikacji np. z Firebase
