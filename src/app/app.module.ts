2cb4f7265b0fa74b17803d239acf86497b8bf6fd
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-03-30 12:07:05 +0200

    bootstrap and ngxbootstrap installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NavbarComponentModule } from '@navigation';
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { AppComponent } from './app.component';
     NavbarComponentModule,
     AngularFireModule.initializeApp(environment.firestoreConfig),
     InMemoryContextStorageModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
